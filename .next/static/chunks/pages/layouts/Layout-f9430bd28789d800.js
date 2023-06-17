236ced9acbaf3af5a8d6e67d1a36c9ad7cfae084
Moayad Abukhadra <moayadabukhadra54@gmail.com>
2023-06-17 16:42:40 +0300

    .

--- .next/static/chunks/pages/layouts/Layout-f9430bd28789d800.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[889],{2312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/layouts/Layout",function(){return _(3541)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2312)}),_N_E=n.O()}]);
