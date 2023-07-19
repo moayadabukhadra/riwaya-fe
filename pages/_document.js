8693c05f469fc79f7038112a87bc88647fc8ae1b
Moayad Abukhadra <97673636+moayadabukhadra@users.noreply.github.com>
2023-07-19 12:16:30 -0400

    .

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, {Head, Html, Main, NextScript} from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import Script from "next/script";
 
 class MyDocument extends Document {
@@ -22,29 +22,40 @@ class MyDocument extends Document {
                         data-ad-client="ca-pub-2029155588392824"
                         async="true"
                         strategy="beforeInteractive"
-                        onError={ (e) => { console.error('Script failed to load', e) }}
+                        onError={(e) => { console.error('Script failed to load', e) }}
                         src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>
-                    <link rel="icon" href="/images/logo/riwaya-logo.png"/>
+                    <link rel="icon" href="/images/logo/riwaya-logo.png" />
                     <Script
                         id="Adsense-id"
                         data-ad-client="ca-pub-2029155588392824"
                         async="true"
                         strategy="beforeInteractive"
-                        onError={ (e) => { console.error('Script failed to load', e) }}
+                        onError={(e) => { console.error('Script failed to load', e) }}
                         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2029155588392824"
                         crossOrigin="anonymous"></Script>
                     <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
                 </Head>
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M5QT6Y6YRH"></Script>
+                <Script id="google-analytics">
+                    {`
+                    window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+    
+                        gtag('config', 'G-M5QT6Y6YRH');
+                        `
+                    }
+                </Script>
                 <body>
-                <Main />
-                <NextScript />
-                <Script
-                    id="Adsense-id"
-                    data-ad-client="ca-pub-2029155588392824"
-                    async="true"
-                    strategy="beforeInteractive"
-                    onError={ (e) => { console.error('Script failed to load', e) }}
-                     src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>
+                    <Main />
+                    <NextScript />
+                    <Script
+                        id="Adsense-id"
+                        data-ad-client="ca-pub-2029155588392824"
+                        async="true"
+                        strategy="beforeInteractive"
+                        onError={(e) => { console.error('Script failed to load', e) }}
+                        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>
                 </body>
             </Html>
         )
