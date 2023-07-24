329867f59d518a5d584940c4eabfdf038d00e428
Moayad Abukhadra <97673636+moayadabukhadra@users.noreply.github.com>
2023-07-24 11:48:13 -0400

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -23,29 +23,21 @@ class MyDocument extends Document {
                         async="true"
                         strategy="beforeInteractive"
                         onError={(e) => { console.error('Script failed to load', e) }}
-                        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>
+                        src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
                     <link rel="icon" href="/images/logo/riwaya-logo.png" />
-                    <Script
-                        id="Adsense-id"
-                        data-ad-client="ca-pub-2029155588392824"
-                        async="true"
-                        strategy="beforeInteractive"
-                        onError={(e) => { console.error('Script failed to load', e) }}
-                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2029155588392824"
-                        crossOrigin="anonymous"></Script>
-                    <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
+                    <Script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js" />
                 </Head>
-                <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M5QT6Y6YRH"></Script>
-                <Script id="google-analytics">
-                    {`
-                    window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-    
-                        gtag('config', 'G-M5QT6Y6YRH');
-                        `
-                    }
-                </Script>
+                <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-M5QT6Y6YRH"></Script>
+                <Script id="google-analytics" strategy="afterInteractive"  dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-M5QT6Y6YRH', {
+            page_path: window.location.pathname,       
+          });
+        `,
+        }}/>
                 <body>
                     <Main />
                     <NextScript />
